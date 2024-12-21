093c65fb425d02f8afa8b38a251b839460018799
TheDeafOne <keegan.woodburn@gmail.com>
2024-12-21 17:13:35 -0500

    extracting text and saving to file. Also logging

--- preprocessor.py
+++ preprocessor.py
@@ -2,8 +2,9 @@ import fitz
 from os import listdir
 import os
 
-UNPARSED_FILES_DIR  = './recipes/unparsed'
-PARSED_FILES_DIR = './recipes/parsed'
+from consts import UNPARSED_FILES_DIR, PARSED_FILES_DIR
+from reader import extract_text_from_img
+
 VALID_FILE_TYPES = ['pdf']
 
 PDF_ZOOM = 10
@@ -18,20 +19,44 @@ def parse_files():
             continue
         
         if file_type == 'pdf':
-            extract_pdf(UNPARSED_FILES_DIR + "/" + file_path)
+            parse_pdf(UNPARSED_FILES_DIR + "/" + file_path)
 
-def extract_pdf(file_path):
+def parse_pdf(file_path):
+    print('parsing pdf:',file_path)
+    
+    # check if parsed dir exists, make it if it doesn't
     file_name = ''.join(file_path.split('.')[:-1]).split('/')[-1]
     parsed_file_dir = PARSED_FILES_DIR + '/' + file_name
     if not os.path.exists(parsed_file_dir):
         os.mkdir(parsed_file_dir)
-    
+
+    # objectify pdf. For each page, make an image of that page, and save it to the parsed dir
+    print('   converting to images...')
     pdf = fitz.open(file_path)
     for page_number in range(len(pdf)):
-        page_image_name = f'{parsed_file_dir}/{page_number+1}-{file_name}.png'
+        print('      page:',page_number)
+        page_img_path = f'{parsed_file_dir}/{page_number+1}-{file_name}.png'
         page = pdf.load_page(page_number)
         pixel_map = page.get_pixmap(matrix=PDF_ZOOM_MATRIX)
-        pixel_map.save(page_image_name)
+        pixel_map.save(page_img_path)
     pdf.close()
 
-parse_files()
+    # extract the text from the image using ocr
+    print('   extracting text...')
+    ocr_text = extract_text_from_img(page_img_path)
+
+    # recipe is illegible, ignore
+    if ocr_text == None:
+        print('done')
+        return
+    
+    print('   saving text...')
+    # save to a txt file in the same parsed dir
+    ocr_text_path = f'{parsed_file_dir}/{page_number+1}-{file_name}.png'
+    file = open(ocr_text_path, 'w')
+    file.write(ocr_text)
+    file.close()
+    print('done\n')
+
+if __name__ == "__main__":
+    parse_files()
--- reader.py
+++ reader.py
@@ -4,18 +4,47 @@ from dotenv import load_dotenv
 
 import pytesseract
 import easyocr
+import prompts  
 from groq import Groq
+from consts import PARSED_FILES_DIR
 
 load_dotenv()
+reader = easyocr.Reader(['en'])
 client = Groq(
     api_key=os.environ.get('LLM_API_KEY'),
 )
 
-def ask_llm(role, content):
+def ask_llm(content, role='user'):
     chat_completion = client.chat.completions.create(
         messages=[dict(role=role, content=content)],
         model=os.environ.get('LLM_MODEL')
     )
     return chat_completion.choices[0].message.content
 
-print('response:',ask_llm('user','hello how are you. Does this work?'))
+
+
+def extract_recipes(recipe_section):
+    image_index = 0
+    unparsed_recipe = ''
+    recipe_image_file_names = os.listdir(recipe_section)
+    
+    while image_index < len(recipe_image_file_names):
+        # extract text
+        img_path = PARSED_FILES_DIR + "/" + recipe_image_file_names[image_index]
+        ocr_text = reader.readtext(img_path, detail=0)
+        unparsed_recipe += ocr_text
+        
+        # check with llm if recipe is complete
+        response = ask_llm(prompts.INITIAL_RECIPE_EXTRACTION_PROMPT.format(ocr_text=unparsed_recipe))
+        print(response)
+        break
+
+
+def extract_text_from_img(img_path):
+    result = reader.readtext(img_path, detail=0)
+    llm_response = ask_llm(prompts.LEGIBILITY_PROMPT.format(ocr_text=result))
+    if prompts.ILLEGIBLE in llm_response:
+        return None
+    
+    return result
+
